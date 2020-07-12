be07a0716d3781c9382b6060fa317b386f76fa7c
hackbluecorp <62452915+hackbluecorp@users.noreply.github.com>
2020-07-12 12:24:57 -0500

    Updates

---
+++ precache-manifest.b5bfc4eb34a27ec56e695a7512a9d82b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bd709430d3b69607080fc27449da256",
+    "url": "/reactdataclient/index.html"
+  },
+  {
+    "revision": "cce7bd509397913dd551",
+    "url": "/reactdataclient/static/css/2.6d92adba.chunk.css"
+  },
+  {
+    "revision": "092024b9bfd16c6c48a0",
+    "url": "/reactdataclient/static/css/main.00aafb5d.chunk.css"
+  },
+  {
+    "revision": "cce7bd509397913dd551",
+    "url": "/reactdataclient/static/js/2.83c3f731.chunk.js"
+  },
+  {
+    "revision": "d44e0cfa1f51b25079454bd515ac8f9b",
+    "url": "/reactdataclient/static/js/2.83c3f731.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "092024b9bfd16c6c48a0",
+    "url": "/reactdataclient/static/js/main.43a5f125.chunk.js"
+  },
+  {
+    "revision": "01ea63c4adddc949975a",
+    "url": "/reactdataclient/static/js/runtime-main.2f0fe440.js"
+  }
+]);
